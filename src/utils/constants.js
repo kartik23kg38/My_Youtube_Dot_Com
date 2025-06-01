669fb3399afbf0a1d9ad30338c9efbf4f87e9d89
kartik23kg38 <guptakartik961@gmail.com>
2025-06-01 06:01:21 +0530

    fix: update YOUTUBE_SEARCH_SUGGESTIONS_API to use HTTPS for improved security

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 const REACT_APP_googleApiKey = import.meta.env.VITE_GOOGLE_API
 export const REACT_APP_YOUTUBE_VDOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + REACT_APP_googleApiKey;
 
-export const YOUTUBE_SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_SUGGESTIONS_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 // Endpoint for search results (used in SearchResults)
 export const YOUTUBE_SEARCH_API = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" + REACT_APP_googleApiKey;
